181d37f749f5d8e29925a537948e8eca4d5eaea5
Selim Çınar <90525336+SELIMCNR@users.noreply.github.com>
2023-01-30 20:45:02 +0300

    Add files via upload

---
+++ Solution1/Apps/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        var arr = new DataStructures.Array.Array<int>(); // Veriyapıları kütüphanesinden Array adlı klasörden obje oluştu
+      
+        arr.Add(23);  
+        arr.Add(55);
+        
+        Console.WriteLine(arr.Count);   // içerisindeki eleman sayısı
+        Console.WriteLine(arr.Capacity);  // nesnenin kapasitesi
+
+        Console.ReadKey();
+    }
+}
---
+++ Solution1/DataStructures/Array/Array.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Array
+{
+    //<T> GENERİC YAPTI , IEnumerable sıralanabilir , ICloneable klonlanabilir,  
+    public class Array<T> : IEnumerable<T>, ICloneable
+    {
+        private T[] InnerList;  // Sınıf içindeki listemiz
+        public int Count { get; set; }  // count özellik dışardan okunabilir sınıf içerisinde yazılabilir.
+        public int Capacity => InnerList.Length; // clasın kapasitesi sınıf içindeki dizinin kapasitesi oldu.
+
+        public Array() // yapılandırıcı metot
+        {
+            InnerList= new T[2];
+            Count=0;
+        }
+        
+        public void Add(T item)  // dışardan classa ekleme yapma metodu 
+        {
+            InnerList[Count]=item;  // listeye ekle item 
+            Count++;
+        }
+
+        public object Clone()
+        {
+            throw new NotImplementedException();
+        }   //klonlanma
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
